49765e527b868b452f0d10b3553c89890fd82835
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-11-01 12:45:35 +0000

    Create Custom Module in Odoo

---
+++ customs/om_hospital/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Hospital Management',
+    'version': '13.0.0.0.1',
+    'category': 'Healthcare',
+    'summary': 'Hospital Management System',
+    'description': 'This module provides a complete solution for managing a hospital.',
+    'license': 'LGPL-3',
+    'depends': ['base', 'mail'],
+    'data': [
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'sequence': 1
+}
