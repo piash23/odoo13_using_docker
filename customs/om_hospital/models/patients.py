44c59c99455fda176ba290479bfedfa7281093e3
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-11-02 04:56:58 +0000

    rec name for Models in Odoo Development

--- customs/om_hospital/models/patients.py
+++ customs/om_hospital/models/patients.py
@@ -4,8 +4,10 @@ from odoo import _, api, fields, models
 class HospitalPatient(models.Model):
     _name = "hospital.patient"
     _description = 'Hospital Patient'
+    # _rec_name = 'patinet_name'
 
     patinet_name = fields.Char(string="Patient Name", required=True)
     patinet_age = fields.Integer(string="Age")
     notes = fields.Text(string='Notes')
     image = fields.Binary(string="Image")
+    name = fields.Char(string="Test")
