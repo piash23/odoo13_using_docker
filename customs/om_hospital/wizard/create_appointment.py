0e7f2972b99130ea9c65c0a0755db6b5cb3a3451
Md. Shihab Uddin <128211124+piash23@users.noreply.github.com>
2023-11-20 10:37:20 +0000

    Create Record From Code in Odoo

--- customs/om_hospital/wizard/create_appointment.py
+++ customs/om_hospital/wizard/create_appointment.py
@@ -7,8 +7,21 @@ class CReateAPpointment(models.TransientModel):
     _name = 'create.appointment'
     _description = 'Create Appointment'
 
-    patient_id = fields.Many2one('hospital.patient', string='Patient')
-    appointment_date = fields.Date(string='Appointment Date')
+    patient_id = fields.Many2one('hospital.patient', string='Patient', required=True)
+    appointment_date = fields.Date(string='Appointment Date', required=True)
 
     def create_appointment(self):
-        pass
+        appointment_id = self.env['hospital.appointment'].create({
+            'patient_id': self.patient_id.id,
+            'appointment_date': self.appointment_date,
+            'notes': 'Created from the wizard',
+        })
+        #return to the newly created appointment
+        return {
+            'name': _('Appointments'),
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'hospital.appointment',
+            'res_id': appointment_id.id,
+            'type': 'ir.actions.act_window',
+        }
